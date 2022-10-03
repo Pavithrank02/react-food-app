a6ea55827123e71999739076c462393419bd0cfd
Pavithran <pavithrank02@gmail.com>
2022-10-03 20:36:42 +0530

    adding indivual meal item and show

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
-
+import Meals from "./components/Meals/Meals";
+import Card from "./components/UI/Card";
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals>
+          <Card></Card>
+        </Meals>
+      </main>
     </Fragment>
   );
 }
