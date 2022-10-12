066a77913384938f8cd9aa81e71f87860307e9a8
Pavithran <pavithrank02@gmail.com>
2022-10-12 23:26:30 +0530

    sending cart data to database

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -22,6 +22,16 @@ const Cart = props => {
         setIsCheckout(true)
 
     }
+    const submitOrderHandler = (userData) => {
+        fetch('https://react-http-78158-default-rtdb.firebaseio.com/orders.json', {
+            method: 'POST',
+            body: JSON.stringify({
+                user: userData,
+                orderedItems: cartCtx.items,
+            })
+        });
+
+    };
 
     const cartItems = <ul className={classes['cart-items']}>
     {cartCtx.items.map(item => 
@@ -47,7 +57,7 @@ const Cart = props => {
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            {isCheckout && <Checkout  onCancel={props.onClose}/>}
+            {isCheckout && <Checkout  onConfirm ={submitOrderHandler} onCancel={props.onClose}/>}
             {!isCheckout && modelActions}
             
         </Modal>
